033535a2de34005cf4bd24ec44e44fef00d6bdf3
rodolfo <rmorac@gmail.com>
2021-03-31 18:42:36 -0300

    agenda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { AgendaComponent } from './agenda/agenda.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CrisisListComponent,
-    HeroesListComponent
+    HeroesListComponent,
+    AgendaComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       {path: 'crisis-list', component: CrisisListComponent},
       {path: 'heroes-list', component: HeroesListComponent},
+      {path: 'agenda', component: AgendaComponent},
       {path: '', redirectTo: '/heroes-list', pathMatch: 'full'},
     ]),
   ],
